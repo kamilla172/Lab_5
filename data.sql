e7ace815be078c00626862b67f1c5425658ecde4
Kamilla <95493196+kamilla172@users.noreply.github.com>
2022-01-13 23:06:42 +0300

    Пояснения разработчика

---
+++ data.sql
@@ -0,0 +1,5 @@
+insert into usr (first_name, middle_name, last_name, email) values
+('Иван', 'Иванович', 'Иванов', 'ivanov@my.example.com'),
+('Петр', 'Петрович', 'Петров', 'petrov@my.example.com'),
+('Иван', 'Петрович', 'Сидоров', 'sidorov@my.example.com');
+SELECT * FROM usr;
