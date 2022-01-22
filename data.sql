c51b2cd3a646075e758aa253a0256ae6e4b51653
Kamilla <95493196+kamilla172@users.noreply.github.com>
2022-01-22 22:29:43 +0300

    Пояснения разработчика

--- data.sql
+++ data.sql
@@ -12,4 +12,15 @@ insert into usr (first_name, middle_name, last_name, email) values
 ('Кирилл', 'Романович', 'Савченко', 'lighting.strike@yandex.ru'),
 ('Камилла', 'Башировна', 'Тебуева', 'Tebueva02@mail.ru'),
 ('Елизавета', 'Владимировна', 'Яковенко', 'lizayakovyenko@gmail.com');
+insert into usr (e_mail, first_name, middle_name, last_name, birth_date, role_name) values
+('m.amenko11@gmail.com', 'Максим', 'Романович', 'Овраменко', '2001-03-06', 'user' ),
+('fsdzvfsd5@gmail.com','Валерий', 'Сергеевич', 'Аграй', '2007-09-08', 'user' ),
+('artemkaiko@bk.ru', 'Артем', 'Евгеньевич', 'Иненко', '2008-09-02', 'user'),
+('karlinalina@mail.ru','Алина', 'Андреевна', 'Карлина', '2000-12-12', 'user' ),
+('serw22a@mail.ru','Сергей', 'Владимирович', 'Островский', '2002-10-11', 'user'),
+('kalvar2@maol.ru','Александр', 'Михайлович', 'Розум','1980-04-06', 'user' ),
+('savincev2000@mail.ru','Александр', 'Вячеславович', 'Савинцев','1981-04-06', 'user'  ),
+('ligstrike@yandex.ru','Кирилл', 'Романович', 'Савченко', '1970-04-06', 'user' ),
+('lizayaenko@gmail.com','Камилла', 'Башировна', 'Тебуева', '1980-05-06', 'user' ),
+('Teba02@mail.ru','Елизавета', 'Владимировна', 'Яковенко', '1960-04-06', 'user' );
 select * from usr;
