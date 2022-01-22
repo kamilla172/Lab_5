e22b3f1d88cdc29cd32f0ce2e04fa6d90b0dc18f
Kamilla <95493196+kamilla172@users.noreply.github.com>
2022-01-22 22:37:22 +0300

    Пояснения разработчика

--- alter_data.sql
+++ alter_data.sql
@@ -2,3 +2,8 @@ Alter table usr rename column email to e_mail;
 alter table usr add constraint email_unique unique(e_mail);
 ALTER TABLE usr ADD COLUMN birth_date timestamp;
 alter table usr alter column birth_date set not null;
+alter table usr add column role_name varchar(20);
+alter table usr alter column role_name set not null;
+alter table usr add constraint FK_USR_ROLE foreign key (role_name) references role;
+alter table role_permission add constraint FK_ROLE_PERMISSION_ROLE foreign key (role_name) references role;
+alter table role_permission add constraint FK_ROLE_PERMISSION_ID foreign key (permission_id) references permission;
