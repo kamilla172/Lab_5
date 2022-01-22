2e78ab3b481e1ee4fa5e73b57fa076e56de9dd40
Kamilla <95493196+kamilla172@users.noreply.github.com>
2022-01-22 15:58:50 +0300

    Пояснения разработчика

--- data.sql
+++ data.sql
@@ -2,4 +2,14 @@ insert into usr (first_name, middle_name, last_name, email) values
 ('Иван', 'Иванович', 'Иванов', 'ivanov@my.example.com'),
 ('Петр', 'Петрович', 'Петров', 'petrov@my.example.com'),
 ('Иван', 'Петрович', 'Сидоров', 'sidorov@my.example.com');
-SELECT * FROM usr;
+('Максим', 'Романович', 'Авраменко', 'm.avramenko11@gmail.com'),
+('Валерий', 'Сергеевич', 'Заграй', 'fsdzvzvzvfsd5@gmail.com'),
+('Артем', 'Евгеньевич', 'Иваненко', 'artemkaivanenko@bk.ru'),
+('Алина', 'Андреевна', 'Карлина', 'karlina_alina@mail.ru'),
+('Сергей', 'Владимирович', 'Островский', 'serw21a@mail.ru'),
+('Александр', 'Михайлович', 'Розум', 'amasarg7@mail.ru'),
+('Александр', 'Вячеславович', 'Савинцев', 'savincev2003@mail.ru'),
+('Кирилл', 'Романович', 'Савченко', 'lighting.strike@yandex.ru'),
+('Камилла', 'Башировна', 'Тебуева', 'Tebueva02@mail.ru'),
+('Елизавета', 'Владимировна', 'Яковенко', 'lizayakovyenko@gmail.com');
+select * from usr;
