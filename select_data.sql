e5a8eacef9ea1b9c3ee48ee9b699e0a120c0475c
Kamilla <95493196+kamilla172@users.noreply.github.com>
2022-02-09 18:49:23 +0300

    Пояснения разработчика

--- select_data.sql
+++ select_data.sql
@@ -1,5 +1,4 @@
-select id, email, first_name, last_name, middle_name from usr where id>7;
-SELECT first_name, last_name FROM usr   WHERE LENGTH (first_name) > 7;
+
 select*from usr where birth_date > '01-01-2005';
 select first_name, last_name, birth_date from usr order by birth_date DESC;
 select* from usr where birth_date>= '01-01-2003' and birth_date <'01-01-2008';
